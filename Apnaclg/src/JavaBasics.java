9ed7775ab7c9161bf0e618e38d445c1eee050f36
Satish Hosur <satishhosur021@gmail.com>
2026-08-25 18:39:22 +0530

    Added factorial program

--- Apnaclg/src/JavaBasics.java
+++ Apnaclg/src/JavaBasics.java
@@ -1,26 +1,15 @@
 import java.util.*;
 public class JavaBasics {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int number ;
-        int choice;
-        int evenSum = 0;
-        int oddSum = 0;
-        do{
-            System.out.println("Enter a number ");
-            number = sc.nextInt();
-          
-            if(number % 2 == 0){
-                evenSum += number;
-            }else{
-                oddSum += number;
-            }
-            System.out.println("Do you want to continue ? if yes press 1 else 0");
-            choice = sc.nextInt();
-
-        }while(choice == 1);
-        System.out.println("Sum of even :" +evenSum);
-        System.out.println("Sum of odd: "+oddSum);
+        Scanner sc =  new Scanner(System.in);
+        int num;
+        int fact = 1;
+        System.out.println("Enter a number");
+        num = sc.nextInt();
+        for( int i =1; i<= num ; i++){
+            fact *= i;
+        }
+       System.out.println(fact);
 
        
        
