11a0196f3264d24b3bb85790a35163fd94099727
Satish Hosur <satishhosur021@gmail.com>
2026-08-26 11:57:00 +0530

    Multiplication program

--- Apnaclg/src/Multiplication.java
+++ Apnaclg/src/Multiplication.java
@@ -2,15 +2,11 @@ import java.util.*;
 public class Multiplication {
    public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    int val = 1;
-    int sum =0;
-    while(val <= n){
-        sum = sum + val ;
-        val++;
-        System.out.println(sum);
-    }
-   
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+        for(int i = 1; i<=10 ; i++){
+        System.out.println(n +"*" +i +"=" +n*i);
+        }
        
         
 
