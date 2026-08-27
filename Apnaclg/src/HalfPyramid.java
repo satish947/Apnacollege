9f6dfacfb5805c595c5c18762567141ea58fc526
Satish Hosur <satishhosur021@gmail.com>
2026-08-27 06:56:23 +0530

    Half Pyramid program

---
+++ Apnaclg/src/HalfPyramid.java
@@ -0,0 +1,13 @@
+public class HalfPyramid {
+    public static void main(String args[]){
+        int  n=4;
+        for(int line =1; line<=4;line++){
+            for(int number =1 ; number <=line;number++){
+                System.out.print(number);
+            
+            }System.out.println();
+        }
+
+    }
+    
+}
