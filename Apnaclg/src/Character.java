816bd9f909bda3298ef53249b7f07b611705315f
Satish Hosur <satishhosur021@gmail.com>
2026-08-27 06:49:06 +0530

    Character pattern

---
+++ Apnaclg/src/Character.java
@@ -0,0 +1,14 @@
+public class Character {
+    public static void main(String[] args){
+        int n=4;
+        char ch = 'A';
+        for(int line =1 ; line<=4;line++){
+            for(int chars=1;chars<=line;chars++){
+                System.out.print(ch);
+                ch++;
+            }System.out.println();
+        }
+        
+    }
+    
+}
