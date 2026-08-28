e0a795478ac56d252c7a9c5160a8fd845d8a9692
Satish Hosur <satishhosur021@gmail.com>
2026-08-28 22:14:02 +0530

    prime numbers between range

--- Apnaclg/src/primeoverlaoding.java
+++ Apnaclg/src/primeoverlaoding.java
@@ -1,14 +1,19 @@
 public class primeoverlaoding {
-    public static boolean isprime(int n ){
-        boolean isprime = true;
-        for(int i =2; i<= n-1 ; i++){
-            if(n % 2 == 0){
-                isprime = false;
-                break;
-            }
-        }return isprime;
-    }public static void main(String args[]){
-        System.out.println(isprime(9));
+  public static boolean isprime(int n){
+    boolean isprime = true;
+    for(int i = 2; i<=n-1 ; i++){
+        if(n%2 == 0){
+            isprime = false;
+        }
+    }return isprime;
+  }
+  public static void primeInrange(int n){
+    for(int i=2 ; i<= n; i++){
+        if(isprime(i)){
+            System.out.print(i);
+        }System.out.println();
     }
-    
+  } public static void main(String args[]){
+    primeInrange(50);
+  }
 }
