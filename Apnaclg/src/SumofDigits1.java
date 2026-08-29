aaead0d524974847a3b8b5c373563c6bc0a352a6
Satish Hosur <satishhosur021@gmail.com>
2026-08-29 23:02:30 +0530

    Sum of Digits

---
+++ Apnaclg/src/SumofDigits1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Sumofdigits {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int digits = sc.nextInt();
+        System.out.println("sum of digits is " + sumDigits(digits));
+    }public static int sumDigits(int n){
+        int sumofDigits =0;
+        while(n>0){
+        int lastDigit = n % 10;
+        sumofDigits += lastDigit;
+        n /= 10;
+        } 
+        return sumofDigits ; 
+    }
+    
+}
