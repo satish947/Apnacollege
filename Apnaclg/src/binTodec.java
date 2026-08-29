5e982a3193e996091e0984ca3ab0f28011625b24
Satish Hosur <satishhosur021@gmail.com>
2026-08-29 18:30:45 +0530

    binary and decimal conversions

--- Apnaclg/src/binTodec.java
+++ Apnaclg/src/binTodec.java
@@ -11,9 +11,26 @@ public class binTodec {
         binNum = binNum/10;
 
     }
-    System.out.println( " decimal to bin of " + myNum + " is " + decNum);
+    System.out.println( " decimal to bin  is " + decNum);
     
-}public static void main(String args[]){
-    binTodec(101);
 }
+public static void decTobin(int n){
+int pow = 0;
+    int binNum =0;
+    while(n>0){
+        int rem = n % 2;
+        binNum= binNum +(rem * (int)Math.pow(10,pow));
+        pow++;
+        n=n/2;
+
+    }
+    System.out.println(" bin to dec is equal to " + binNum);
+
 }
+    public static void main(String args[]){
+        binTodec(010);
+        decTobin(7);
+    }
+}
+   
+
