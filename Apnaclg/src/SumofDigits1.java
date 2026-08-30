b2f7bfca37eb54a97421452b092c1784abf7f836
Satish Hosur <satishhosur021@gmail.com>
2026-08-30 23:06:27 +0530

    inverted half pyramid of numbers

--- Apnaclg/src/SumofDigits1.java
+++ Apnaclg/src/SumofDigits1.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Sumofdigits {
+public class SumofDigits1 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int digits = sc.nextInt();
