e591e6f8ab5d9dad8f581c5eb108a399bf295bd2
Satish Hosur <satishhosur021@gmail.com>
2026-09-07 18:25:05 +0530

    matrix sum of rowa

---
+++ Apnaclg/src/number.java
@@ -0,0 +1,24 @@
+public class number {
+    public static void sum(int matrix[][]){
+      
+        int num = 0;
+        for(int row = 0 ; row<matrix.length;row++){
+            for(int col = 0 ; col< matrix[0].length ;col++){
+        if(matrix[row][col] == 7){
+            num += 1;
+        }
+       
+       
+    }
+     
+
+    }
+     System.out.println(num);
+     
+}
+    public static void main(String args[]){
+        int matrix[][] ={{4,7,8},{8,8,7},{7,7,5}};
+        sum(matrix);
+    }
+    
+}
---
+++ Apnaclg/src/rowsum.java
@@ -0,0 +1,18 @@
+public class rowsum {
+    public static void sumofrow(int matrix[][]){
+        int sum = 0;
+        for(int row =1;row<2 ; row++){
+            for(int col = 0 ; col<matrix[0].length ;col++){
+                sum += matrix[row][col];
+            }
+        }
+        System.out.print(sum);
+    }
+    public static void main(String args[]){
+        int matrix[][] = {{1,4,9},{11,4,3},{2,2,3}};
+         sumofrow(matrix);
+
+    }
+
+    
+}
