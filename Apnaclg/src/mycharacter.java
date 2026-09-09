32d53b55535fa3ce859a3d382ab1912f116f3818
Satish Hosur <satishhosur021@gmail.com>
2026-09-09 22:18:25 +0530

    String compress

--- Apnaclg/src/mycharacter.java
+++ Apnaclg/src/mycharacter.java
@@ -7,9 +7,9 @@ public class mycharacter {
         char Ch = java.lang.Character.toUpperCase(str.charAt(0));
         sb.append(Ch);
 
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 1; i < str.length(); i++) {
 
-            if (str.charAt(i) == ' ' && i < str.length() - 1) {
+            if (str.charAt(i) == ' ' && i < str.length()-1) {
                 i++;
                 sb.append(java.lang.Character.toUpperCase(str.charAt(i)));
             } else {
