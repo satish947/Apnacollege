b9ec17ed26b58a46b610a469aa3445e0f5d741cf
Satish Hosur <satishhosur021@gmail.com>
2026-09-09 22:39:33 +0530

    counting vowels

---
+++ Apnaclg/src/vowels.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class vowels {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        
+        int count = 0;
+
+        
+        for(int i = 1; i<str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch == 'a' || ch == 'e'|| ch== 'i'|| ch=='o'||ch == 'u'){
+                count += 1;
+            }
+        }
+         System.out.println(count);
+    }
+    
+}
